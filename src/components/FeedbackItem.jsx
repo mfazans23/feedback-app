54407a6ef46e9f71168823682dc8d0ebfe44b5b2
mfazans23 <mfaza16717@mail.ugm.ac.id>
2022-01-09 03:36:48 +0700

    Modified FeedbackItem & Header components

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,9 +1,4 @@
-import { useState } from 'react'
-
-function FeedbackItem() {
-  const [rating, setRating] = useState(7)
-  const [text, setText] = useState('This is an example for feedback item')
-
+function FeedbackItem({ rating, text }) {
   return (
     <div className='card'>
       <div className='num-display'>{rating}</div>
