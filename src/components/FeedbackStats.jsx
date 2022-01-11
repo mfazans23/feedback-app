f7aa6a1eb653b8dd311a5f3eeb81351ff54a9342
mfazans23 <mfaza16717@mail.ugm.ac.id>
2022-01-11 16:17:33 +0700

    Add proptypes to FeedbackStats component

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,14 +1,21 @@
-function FeedbackStats({ feedback }) {
-  const feedbackLength = feedback.length
-  const average =
-    feedback.reduce((sum, item) => sum + item.rating, 0) / feedbackLength
+import PropTypes from 'prop-types'
 
+function FeedbackStats({ feedback }) {
+  // Calculate ratings avg
+  let average =
+    feedback.reduce((acc, cur) => acc + cur.rating, 0) / feedback.length
+
+  average = average.toFixed(1).replace(/[.,]0$/, '')
   return (
     <div className='feedback-stats'>
-      <h4>The number of feedback: {feedback.length}</h4>
-      <h4>The average rating: {average}</h4>
+      <h4>{feedback.length} reviews</h4>
+      <h4>The average rating: {isNaN(average) ? 0 : average}</h4>
     </div>
   )
+}
+
+FeedbackStats.propTypes = {
+  feedback: PropTypes.array.isRequired,
 }
 
 export default FeedbackStats
